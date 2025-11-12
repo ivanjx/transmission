7e5211a31a3b73486ecd3e43186c57baeadfe88e
Ivan Kara <ivanjuan222@gmail.com>
2025-11-12 13:22:30 +0700

    Revert "Remove country code caching"
    This reverts commit 2769386dbcf7f143a2cb8bd08924b6c9f627bc9c.

--- web/src/inspector.js
+++ web/src/inspector.js
@@ -20,6 +20,9 @@ const peer_column_classes = [
 ];
 
 export class Inspector extends EventTarget {
+  // Simple cache for IP -> country code mappings
+  static _countryCache = new Map();
+
   constructor(controller) {
     super();
 
@@ -997,7 +1000,13 @@ export class Inspector extends EventTarget {
 
   static async _fetchCountryCode(ip) {
     try {
-      const response = await fetch(`https://api.country.is/${ip}`); // has caching
+      // Check cache first
+      if (Inspector._countryCache.has(ip)) {
+        return Inspector._countryCache.get(ip);
+      }
+
+      // Use api.country.is API to get country code
+      const response = await fetch(`https://api.country.is/${ip}`);
 
       if (!response.ok) {
         return null;
@@ -1005,6 +1014,12 @@ export class Inspector extends EventTarget {
 
       const data = await response.json();
       const countryCode = data.country || null;
+
+      // Cache the result (only if we got a valid response)
+      if (countryCode) {
+        Inspector._countryCache.set(ip, countryCode);
+      }
+
       return countryCode;
     }
     catch (error) {
